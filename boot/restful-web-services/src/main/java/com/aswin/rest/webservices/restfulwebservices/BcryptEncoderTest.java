69954e844396aef08ce266ea02939f576f264bde
unknown <aswinthekepoyil@gmail.com>
2020-08-19 19:26:10 +0530

    initial commit

---
+++ boot/restful-web-services/src/main/java/com/aswin/rest/webservices/restfulwebservices/BcryptEncoderTest.java
@@ -0,0 +1,19 @@
+package com.aswin.rest.webservices.restfulwebservices;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class BcryptEncoderTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		
+		for(int i=0; i<=10; i++) {
+			String encodedString = encoder.encode("tptp");
+			System.out.println(encodedString);
+		}
+		
+
+	}
+
+}
